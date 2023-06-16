46340ccabd39209dee077892a696f4782de680fe
BoltR6 <johnwallacewhite@gmail.com>
2023-06-16 17:04:10 -0500

    Migrated to Iced framework

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
+// Iced garbage
+use iced::executor;
+use iced::widget::canvas::{
+    stroke, Cache, Cursor, Geometry, LineCap, Path, Stroke,
+};
+use iced::widget::{ canvas, container, Image, };
+use iced::{
+    Application, Color, Command, Element, Length, Point, Rectangle, Settings,
+    Subscription, Theme, Vector, 
+};
+
 use std::fs;
 use core::slice::Iter;
+use std::vec::IntoIter;
 use std::collections::HashMap;
 use regex::Regex;
 
-use nannou::prelude::*;
 
 struct Character<'a> {
     name: &'a str,
@@ -24,116 +35,218 @@ enum Transition {
     End()
 }
 
-struct VisualNovel<'a> {
-    transitions_iter: Iter<'a, Transition>,
+struct VisualNovel {
+    //backgrounds: HashMap<String, wgpu::Texture>,
+
+    transitions_iter: IntoIter<Transition>,
     current_background: String,
+    
+    now: time::OffsetDateTime,
+    clock: Cache,
 }
 
-struct Model {
-    backgrounds: HashMap<String, wgpu::Texture>,
+pub fn main() -> iced::Result {
+    VisualNovel::run(Settings {
+        antialiasing: true,
+        ..Settings::default()
+    })
 }
 
-fn main() {
-    let command_structure = Regex::new(r"(\w+)(?: (\w+)\=`(.+?)`)+").unwrap();
-
-    // Compile Script into a vector Transitions, then create an iterator over them
-    let full_script_string: String = fs::read_to_string("./assets/scripts/script.txt")
-        .expect("Issue reading file!");
-    let transitions: Vec<Transition> = full_script_string.lines().map(|line| {
-        println!("[ Compiling ] `{line}`");
-
-        let mut command_options: HashMap<String, String> = HashMap::new();
-
-        // Grabs the command in its normal habitat
-        let command_captures = command_structure.captures(line).unwrap();
-
-        // Remove the command identifier seperately
-        let mut args = command_captures.iter();
-        let cmd_id = args
-            .nth(1)
-            .expect("There should be a command definition.")
-            .expect("There should be a match on the first.")
-            .as_str();
-        println!("CMD: `{cmd_id}`");
-
-        // Adds each option from the command to the options hashmap
-        while let Some(capture) = args.next() {
-            let option: String = capture.map_or("".to_string(), |m| m.as_str().to_string());
-            let value: String  = args.next().expect("Missing value!").map_or("".to_string(), |m| m.as_str().to_string());
-            
-            command_options.insert(option, value);
-        }
-
-        // Try to run the command
-        match cmd_id {
-            "log" => {
-                return Transition::Log(command_options.get("msg").expect("Should have value!").to_string());
-            },
-            _ => panic!("Bad command! {cmd_id}")
-        }
-    }).collect();
-    let transitions_iter = transitions.iter();
-
-    let primary = VisualNovel {
-        transitions_iter,
-        current_background: "default".to_string(),
-    };
-
-    nannou::app(model)
-        .update(update)
-        .run();
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Tick(time::OffsetDateTime),
 }
 
-fn model(app: &App) -> Model {
-    app.new_window().size(1280, 800).view(view).build().unwrap();
+impl Application for VisualNovel {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Theme = Theme;
+    type Flags = ();
 
-    let assets = app.assets_path().unwrap();
+    fn new(_flags: ()) -> (Self, Command<Message>) {
+        /* WARM UP ASSETS */
 
-    // Literal Asset Hashmaps
-    let mut backgrounds = HashMap::new();
+        // Literal Asset Hashmaps
+        /*
+        let mut backgrounds = HashMap::new();
 
+        let background_paths = fs::read_dir(assets.join("images").join("backgrounds"))
+            .expect("No backgrounds dir!")
+            .map(|entry| entry.unwrap().path());
+        for background_path in background_paths {
+            let file_name = background_path
+                .file_name().unwrap()
+                .to_str().unwrap()
+                .to_string();
+            let file_texture = wgpu::Texture::from_path(app, background_path).unwrap();
+            backgrounds.insert(file_name, file_texture);
+        }*/
 
-    let background_paths = fs::read_dir(assets.join("images").join("backgrounds"))
-        .expect("No backgrounds dir!")
-        .map(|entry| entry.unwrap().path());
-    for background_path in background_paths {
-        let file_name = background_path
-            .file_name().unwrap()
-            .to_str().unwrap()
-            .to_string();
-        let file_texture = wgpu::Texture::from_path(app, background_path).unwrap();
-        backgrounds.insert(file_name, file_texture);
+        /* PRECOMPILATION */
+        let command_structure = Regex::new(r"(\w+)(?: (\w+)\=`(.+?)`)+").unwrap();
+
+        // Compile Script into a vector Transitions, then create an iterator over them
+        let full_script_string: String = fs::read_to_string("./assets/scripts/script.txt")
+            .expect("Issue reading file!");
+        let transitions: Vec<Transition> = full_script_string.lines().map(move |line| {
+            println!("[ Compiling ] `{line}`");
+
+            let mut command_options: HashMap<String, String> = HashMap::new();
+
+            // Grabs the command in its normal habitat
+            let command_captures = command_structure.captures(line).unwrap();
+
+            // Remove the command identifier seperately
+            let mut args = command_captures.iter();
+            let cmd_id = args
+                .nth(1)
+                .expect("There should be a command definition.")
+                .expect("There should be a match on the first.")
+                .as_str();
+            println!("CMD: `{cmd_id}`");
+
+            // Adds each option from the command to the options hashmap
+            while let Some(capture) = args.next() {
+                let option: String = capture.map_or("".to_string(), |m| m.as_str().to_string());
+                let value: String  = args.next().expect("Missing value!").map_or("".to_string(), |m| m.as_str().to_string());
+                
+                command_options.insert(option, value);
+            }
+
+            // Try to run the command
+            match cmd_id {
+                "log" => {
+                    return Transition::Log(command_options.get("msg").expect("Should have value!").to_string());
+                },
+                _ => panic!("Bad command! {cmd_id}")
+            }
+        }).collect();
+
+        let visual_novel = VisualNovel {
+            //backgrounds,
+
+            transitions_iter: transitions.into_iter(),
+
+            current_background: "default".to_string(),
+
+            now: time::OffsetDateTime::now_local()
+                .unwrap_or_else(|_| time::OffsetDateTime::now_utc()),
+            clock: Default::default(),
+        };
+        (
+            visual_novel,
+            Command::none(),
+        )
     }
 
+    fn title(&self) -> String {
+        String::from("Ettethread - Requiem")
+    }
 
-    
-    
-    
-    Model { backgrounds }
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::Tick(local_time) => {
+                let now = local_time;
+
+                if now != self.now {
+                    self.now = now;
+                    self.clock.clear();
+                }
+            }
+        }
+
+        Command::none()
+    }
+
+    fn view(&self) -> Element<Message> {
+        let canvas = canvas(self as &Self)
+            .width(Length::Fill)
+            .height(Length::Fill);
+
+        container(canvas)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .padding(20)
+            .into()
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        iced::time::every(std::time::Duration::from_millis(500)).map(|_| {
+            Message::Tick(
+                time::OffsetDateTime::now_local()
+                    .unwrap_or_else(|_| time::OffsetDateTime::now_utc()),
+            )
+        })
+    }
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
+impl<Message> canvas::Program<Message> for VisualNovel {
+    type State = ();
 
-fn view(app: &App, model: &Model, frame: Frame) {
+    fn draw(
+        &self,
+        _state: &Self::State,
+        _theme: &Theme,
+        bounds: Rectangle,
+        _cursor: Cursor,
+    ) -> Vec<Geometry> {
+        let clock = self.clock.draw(bounds.size(), |frame| {
+            let center = frame.center();
+            let radius = frame.width().min(frame.height()) / 2.0;
 
-    // Prepare to draw.
-    let draw = app.draw();
+            let background = Path::circle(center, radius);
+            frame.fill(&background, Color::from_rgb8(0x12, 0x93, 0xD8));
 
-    // Generate sine wave data based on the time of the app
-    let sine = app.time.sin();
-    let slowersine = (app.time / 2.0).sin();
+            let short_hand =
+                Path::line(Point::ORIGIN, Point::new(0.0, -0.5 * radius));
 
-    // Get boundary of the window (to constrain the movements of our circle)
-    let boundary = app.window_rect();
+            let long_hand =
+                Path::line(Point::ORIGIN, Point::new(0.0, -0.8 * radius));
 
-    // Map the sine wave functions to ranges between the boundaries of the window
-    let x = map_range(sine, -1.0, 1.0, boundary.left(), boundary.right());
-    let y = map_range(slowersine, -1.0, 1.0, boundary.bottom(), boundary.top());
+            let width = radius / 100.0;
 
-    draw.texture(model.backgrounds.get(primary.current_background).expect("Background does not exist!"));
+            let thin_stroke = || -> Stroke {
+                Stroke {
+                    width,
+                    style: stroke::Style::Solid(Color::WHITE),
+                    line_cap: LineCap::Round,
+                    ..Stroke::default()
+                }
+            };
 
-    // Draw a blue ellipse at the x/y coordinates 0.0, 0.0
-    draw.ellipse().color(STEELBLUE).x_y(x, y);
+            let wide_stroke = || -> Stroke {
+                Stroke {
+                    width: width * 3.0,
+                    style: stroke::Style::Solid(Color::WHITE),
+                    line_cap: LineCap::Round,
+                    ..Stroke::default()
+                }
+            };
 
-    draw.to_frame(app, &frame).unwrap();
+            frame.translate(Vector::new(center.x, center.y));
+
+            frame.with_save(|frame| {
+                frame.rotate(hand_rotation(self.now.hour(), 12));
+                frame.stroke(&short_hand, wide_stroke());
+            });
+
+            frame.with_save(|frame| {
+                frame.rotate(hand_rotation(self.now.minute(), 60));
+                frame.stroke(&long_hand, wide_stroke());
+            });
+
+            frame.with_save(|frame| {
+                frame.rotate(hand_rotation(self.now.second(), 60));
+                frame.stroke(&long_hand, thin_stroke());
+            })
+        });
+
+        vec![clock]
+    }
+}
+
+fn hand_rotation(n: u8, total: u8) -> f32 {
+    let turns = n as f32 / total as f32;
+
+    2.0 * std::f32::consts::PI * turns
 }
