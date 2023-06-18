6036c7f27bbe6a07a1015f238330647ca73343b7
BoltR6 <johnwallacewhite@gmail.com>
2023-06-18 16:01:14 -0500

    Set emotion system implemented

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,6 @@ use json::parse;
 
 #[derive(Resource, Default)]
 struct VisualNovelState {
-    //backgrounds: HashMap<String, image::Handle>,
-
     transitions_iter: IntoIter<Transition>,
     blocking: bool,
     current_background: String,
@@ -72,7 +70,7 @@ fn import_characters(mut commands: Commands, asset_server: Res<AssetServer>){
             .map(|entry| entry.unwrap().path());
         for sprite_path in sprite_paths {
             let sprite_name = sprite_path
-                .file_name().unwrap()
+                .file_stem().unwrap()
                 .to_str().unwrap()
                 .to_string();
             let file_texture = asset_server.load(sprite_path);
@@ -149,21 +147,41 @@ fn update_characters(
 enum Transition {
     Background(String),
     Say(String, String),
+    SetEmotion(String, String),
     Log(String),
     End
 }
 impl Transition {
-    fn call(&self, game_state: &mut ResMut<VisualNovelState> ) {
+    fn call(&self, game_state: &mut ResMut<VisualNovelState>, character_query: &mut Query<(
+        &mut Character, 
+        &CharacterSprites,
+        &mut Handle<Image>
+    )> ) {
         match self {
             Transition::Background(id) => {
                 (*game_state).current_background = id.clone();
-                println!("Set current background to '{id}'");
+                println!("[ Set current background to '{id}' ]");
             },
             Transition::Say(_character_name, _msg) => {
                 todo!();
             },
+            Transition::SetEmotion(character_name, emotion) => {
+                for (mut character, sprites, mut current_sprite) in character_query.iter_mut() {
+                    if character.name == *character_name {
+                        character.emotion = emotion.to_owned();
+                        *current_sprite = sprites.outfits.get(&character.outfit)
+                            .expect("'{character.outfit}' attribute does not exist!")
+                            .get(&character.emotion)
+                            .expect("'default_emotion' atttribute does not exist!")
+                            .clone();
+                        println!("[ Set emotion of '{character_name}' to '{emotion}']");
+                    }
+                }
+            }
             Transition::Log(msg) => println!("{msg}"),
-            Transition::End => println!("TODO: END")
+            Transition::End => {
+                todo!();
+            }
         }
     }
 }
@@ -177,8 +195,8 @@ impl Plugin for Compiler {
 }
 fn pre_compile( mut game_state: ResMut<VisualNovelState>){
     /* PRECOMPILATION */
-    let command_structure = Regex::new(r"(\w+)(?: (\w+)\=`(.+?)`)+").unwrap();
-
+    let command_structure = Regex::new(r"(\w+)[\s$]").unwrap();
+    let argument_structure = Regex::new(r"(\w+)=`([^`]*)`").unwrap();
     // Compile Script into a vector Transitions, then create an iterator over them
     let full_script_string: String = fs::read_to_string(std::env::current_dir()
             .expect("Failed to get current directory!")
@@ -191,23 +209,25 @@ fn pre_compile( mut game_state: ResMut<VisualNovelState>){
 
         let mut command_options: HashMap<String, String> = HashMap::new();
 
-        // Grabs the command in its normal habitat
-        let command_captures = command_structure.captures(line)
-            .expect("Bad command structure! Ex: [cmd option1=`` option2=``]");
-
         // Remove the command identifier seperately
-        let mut args = command_captures.iter();
-        let cmd_id = args
+        let cmd_id = command_structure.captures_iter(line)
+            .next()
+            .unwrap()
+            .iter()
             .nth(1)
-            .expect("There should be a command definition.")
-            .expect("There should be a match on the first.")
+            .unwrap()
+            .expect("??")
             .as_str();
         println!("CMD: `{cmd_id}`");
 
+        
         // Adds each option from the command to the options hashmap
+        let mut args = argument_structure.captures_iter(line);
         while let Some(capture) = args.next() {
-            let option: String = capture.map_or(String::from(""), |m| m.as_str().to_owned());
-            let value: String  = args.next().expect("Missing value!").map_or(String::from(""), |m| m.as_str().to_owned());
+            let mut argument = capture.iter();
+            println!("Field - {}", argument.next().unwrap().unwrap().as_str());
+            let option: String = argument.next().expect("Missing field!").map_or(String::from(""), |m| m.as_str().to_owned());
+            let value: String  = argument.next().expect("Missing value!").map_or(String::from(""), |m| m.as_str().to_owned());
             
             command_options.insert(option, value);
         }
@@ -235,6 +255,23 @@ fn pre_compile( mut game_state: ResMut<VisualNovelState>){
                     .to_owned();
                 return Transition::Say(character_id, msg);
             },
+            "set" => {
+                let type_of = command_options.get("type")
+                    .expect("Missing 'type' option!")
+                    .as_str();
+                match type_of {
+                    "emotion" => {
+                        let character_name = command_options.get("character")
+                            .expect("Missing 'character' option!")
+                            .to_owned();
+                        let emotion = command_options.get("emotion")
+                            .expect("Missing 'emotion' option!")
+                            .to_owned();
+                        return Transition::SetEmotion(character_name, emotion);
+                    },
+                    _ => panic!("Bad type '{type_of}'!")
+                }
+            }
             "end" => {
                 return Transition::End;
             }
@@ -247,14 +284,21 @@ fn pre_compile( mut game_state: ResMut<VisualNovelState>){
 
     println!("[ Completed Compilation ]");
 }
-fn run_transitions ( mut game_state: ResMut<VisualNovelState> ) {
+fn run_transitions ( 
+    mut game_state: ResMut<VisualNovelState>, 
+    mut character_query: Query<(
+        &mut Character, 
+        &CharacterSprites,
+        &mut Handle<Image>
+    )>
+) {
     loop {
         if game_state.blocking {
             return;
         }
         match game_state.transitions_iter.next() {
             Some(transition) => {
-                transition.call(&mut game_state);
+                transition.call(&mut game_state, &mut character_query);
             },
             None => {
                 return;
