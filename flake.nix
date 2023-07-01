9afc645d9a6d5605360e269a1a184416397c266e
BoltR6 <johnwallacewhite@gmail.com>
2023-07-01 12:47:48 -0500

    Github Actions - Done?

--- flake.nix
+++ flake.nix
@@ -12,16 +12,54 @@
           inherit system;
           overlays = [ rust-overlay.overlays.default ];
         };
-        rustVersion = "latest";
-        rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
-          extensions = [
-            "rust-src" # for rust-analyzer
-          ];
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+
+        bevyRustBuild = rustPlatform.buildRustPackage rec {
+          pname = "app";
+          version = "0.0.1";
+          src = ./.;
+          cargoBuildFlags = "";
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = [ (rustVersion.override { extensions = ["rust-src"]; }) ] ++ (with pkgs; [ 
+            pkg-config
+            cargo
+            gcc
+            rustfmt
+            clippy
+          ]);
+          buildInputs = (with pkgs; [ 
+            udev
+            alsa-lib
+            vulkan-loader
+            libxkbcommon
+            wayland
+          ]) ++ (with pkgs.xorg; [
+            libX11
+            libXcursor 
+            libXrandr
+            libXi
+          ]);
+
+          # Certain Rust tools won't work without this
+          # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+          # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
         };
       in
       {
+        packages.ermmmmm = bevyRustBuild;
         devShell = pkgs.mkShell rec {
-          nativeBuildInputs = [ rust ] ++ (with pkgs; [ 
+          nativeBuildInputs = [ (rustVersion.override { extensions = ["rust-src"]; }) ] ++ (with pkgs; [ 
             pkg-config
             cargo
             gcc
