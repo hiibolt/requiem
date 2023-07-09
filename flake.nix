fcc1375e4497bdec87837555741b2cd84fe82f5f
BoltR6 <johnwallacewhite@gmail.com>
2023-07-08 23:24:33 -0500

    Don't need devShell, use `nix develop .#ermmmmm`

--- flake.nix
+++ flake.nix
@@ -58,32 +58,5 @@
       in
       {
         packages.ermmmmm = bevyRustBuild;
-        devShell = pkgs.mkShell rec {
-          nativeBuildInputs = [ (rustVersion.override { extensions = ["rust-src"]; }) ] ++ (with pkgs; [ 
-            pkg-config
-            cargo
-            gcc
-            rustfmt
-            clippy
-          ]);
-          buildInputs = (with pkgs; [ 
-            udev
-            alsa-lib
-            vulkan-loader
-            libxkbcommon
-            wayland
-          ]) ++ (with pkgs.xorg; [
-            libX11
-            libXcursor 
-            libXrandr
-            libXi
-          ]);
-
-          # Certain Rust tools won't work without this
-          # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
-          # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
-          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-        };
     });
 }
