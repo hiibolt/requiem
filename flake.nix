77077ede863b4baad12722132042e14506997fc4
hiibolt <my_git_username@gmail.com>
2024-10-31 15:22:36 -0500

    feat: Fixed and updated `flake.nix` to work with NixOS

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
           rustc = rustVersion;
         };
 
-        bevyRustBuild = rustPlatform.buildRustPackage rec {
+        localRustBuild = rustPlatform.buildRustPackage rec {
           pname = "app";
           version = "0.0.1";
           src = ./.;
@@ -35,28 +35,43 @@
             gcc
             rustfmt
             clippy
+            openssl.dev
           ]);
           buildInputs = (with pkgs; [ 
+            # Audio and Input
             udev
             alsa-lib
+
+            # Vulkan + OpenGL
+            libGL
+            vulkan-tools
+            vulkan-headers
             vulkan-loader
+            vulkan-validation-layers
             libxkbcommon
+
+            # GTK + Wayland
             wayland
+            gtk3-x11
+            gtk3
           ]) ++ (with pkgs.xorg; [
             libX11
             libXcursor 
             libXrandr
             libXi
+            libxcb
           ]);
 
           # Certain Rust tools won't work without this
           # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
           # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
           RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          OPENSSL_LIB_DIR = pkgs.openssl.out + "/lib";
         };
       in
       {
-        packages.ermmmmm = bevyRustBuild;
+        defaultPackage = localRustBuild;
     });
 }
